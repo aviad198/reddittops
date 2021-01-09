f745fd3929e0b6def90e8ec6e6809be761cdafdf
aviad198 <aviad198@gmail.com>
2021-01-09 23:43:21 +0200

    Start implementing RestAPI

--- server.js
+++ server.js
@@ -14,19 +14,20 @@ app.get('/subreddit/:name', (req, res) => {
 
     fetch(`https://www.reddit.com/r/${name}/top.json`).then(response => response.json())
         .then(data => {
-            data.data.children.map((sub, i) => {arr.push(
-                key = i,
-                title = sub.data.title,
-                url = sub.data.url)
+            data.data.children.map((sub, i) => {
+                arr.push(
+                    key = i,
+                    title = sub.data.title,
+                    url = sub.data.url)
             })
-        }).then(subs => {
-        if (arr.length) {
-            res.status(200).json(arr)
-        } else {
-            res.status(400).json('Not found')
-        }
-    })
-        .catch(err => res.status(400).json('error getting user'))
+        }).catch(err => res.status(400).json('error getting user'))
+
+    if (arr.length) {
+        res.status(200).json(arr)
+    } else {
+        res.status(400).json('Not found')
+    }
+
 })
 
 app.listen(3001);
